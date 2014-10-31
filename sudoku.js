57684e760c5d48fb6f8a606cb8de648e2fef4b66
alanflorendo <alan.florendo@gmail.com>
2014-10-31 12:02:02 -0400

    board object represents entire sudoku board

--- sudoku.js
+++ sudoku.js
@@ -1,25 +1,32 @@
+
 $(document).ready(function(){
 	$("#load_board").click(function(){
 		var bdStr = $("#sudoku_string").val();
-		loadBoardToDOM(bdStr);
+		sBd = new SudokuBoard(bdStr)
+		sBd.loadBoardToDOM();
 	})
 })
 
-function loadBoardToDOM(boardString) {
-	for (var i=0; i<9; i++){
-		for (var j=0; j<9; j++){
-			var index = i*9 + j;
-			var cell = new Cell(boardString[index]);
-			var selector = "#board tr:nth-child(" + (i+1) + ") td:nth-child(" + (j+1) + ")";
-			cell.selector = $(selector);
+function SudokuBoard(boardString) {
+	this.boardString = boardString;
 
-			cell.rowNum = i;
-			cell.colNum = j;
-			cell.cageNum = (Math.floor((i-1) / 3) * 3) + Math.floor((j-1) / 3);
-			
-			cell.selector.html(boardString[i*9+j]);
+	this.loadBoardToDOM = function() {
+		for (var i=0; i<9; i++){
+			for (var j=0; j<9; j++){
+				var index = i*9 + j;
+				var cell = new Cell(this.boardString[index]);
+				var selector = "#board tr:nth-child(" + (i+1) + ") td:nth-child(" + (j+1) + ")";
+				cell.selector = $(selector);
+
+				cell.rowNum = i;
+				cell.colNum = j;
+				cell.cageNum = (Math.floor((i-1) / 3) * 3) + Math.floor((j-1) / 3);
+				
+				cell.selector.html(cell.value);
+			}
 		}
 	}
+
 }
 
 function Cell(num) {
