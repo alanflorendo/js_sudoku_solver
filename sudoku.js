3dc12e02ebb03165da4c4c9d8c4c17219d3a0a5e
alanflorendo <alan.florendo@gmail.com>
2014-10-31 11:59:34 -0400

    loadBoardToDOM now works using cell objects with properties (value, row, col, cage, selector)

--- sudoku.js
+++ sudoku.js
@@ -8,8 +8,26 @@ $(document).ready(function(){
 function loadBoardToDOM(boardString) {
 	for (var i=0; i<9; i++){
 		for (var j=0; j<9; j++){
+			var index = i*9 + j;
+			var cell = new Cell(boardString[index]);
 			var selector = "#board tr:nth-child(" + (i+1) + ") td:nth-child(" + (j+1) + ")";
-			$(selector).html(boardString[i*9+j]);
+			cell.selector = $(selector);
+
+			cell.rowNum = i;
+			cell.colNum = j;
+			cell.cageNum = (Math.floor((i-1) / 3) * 3) + Math.floor((j-1) / 3);
+			
+			cell.selector.html(boardString[i*9+j]);
 		}
 	}
+}
+
+function Cell(num) {
+	this.value = num;
+	this.rowNum;
+	this.colNum;
+	this.cageNum;
+	this.indexNum;
+	this.candidates = [];
+	this.selector;
 }
