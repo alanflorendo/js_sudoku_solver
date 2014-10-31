58705f22b7c3dc1bc916699ca9398188bfce8167
alanflorendo <alan.florendo@gmail.com>
2014-10-31 17:08:32 -0400

    logCandidate logs cells soved by candidate

--- sLog.js
+++ sLog.js
@@ -2,6 +2,24 @@ function sLog(game) {
 	this.game = game;
 	this.logNo = 1;
 
+	this.processComment = function (type, cellNum, value) {
+		switch(type) {
+			case "candidate": 
+				this.logCandidate(cellNum, value);
+				break;
+		}
+	}
+
+	this.logCandidate = function(cellNum, value) {
+		var cell = this.game.cells[cellNum];
+		var rowStr = "R" + this.game.rowVals[cell.rowNum].join("");
+		var colStr = "C" + this.game.colVals[cell.colNum].join("");
+		var cageStr = "B" + this.game.cageVals[cell.cageNum].join("");
+		var statStr = rowStr + colStr + cageStr;
+		var str = "CAND: #" + cellNum + " (" + value + "). " + statStr;
+		this.log(str, "blue");
+	}
+
 	this.log = function(text, color) {
 		var logHTML = '<li class="'+ color +'">' + this.logNo + '. ' + text + '</li>';
 		$("#sLog uL").prepend(logHTML);
